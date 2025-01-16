86014bbacbc2a6949f2a378e76abc16b6501198d
Romário Oliveira <romarinhomito27@gmail.com>
2025-01-16 17:27:04 -0300

    Add files via upload

---
+++ ESTUDO SQL UNION.sql
@@ -0,0 +1,14 @@
+--  CURSO SQL OPERADOR UNION QUE NO CASO AJUDA COMBINAR DOIS OU MAIS DADOS DE UM SELECT EM  UM  SÓ RESULTADO 
+
+SELECT Identificacao, 
+       LOGIN, 
+       Email_senha
+FROM Operadors
+WHERE Identificacao LIKE '%SO%'
+UNION  -- OS DUPLICADOS É REMOVIDO AO NÃO SER QUE USE O ALL NA FRENTE DO COMANDO 
+
+SELECT Nome, 
+       Descricao, 
+       NULL AS Email_senha  -- Adiciona a coluna ausente como NULL
+FROM LOJAS 
+WHERE Nome LIKE '%4%'
